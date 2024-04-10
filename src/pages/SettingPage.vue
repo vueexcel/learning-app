9da4e3b233b9bbec73ac74c017932a4ec6d07eaa
vueexcel <vue.excel2011@gmail.com>
2024-04-10 11:17:29 +0530

    added pinia

--- src/pages/SettingPage.vue
+++ src/pages/SettingPage.vue
@@ -1,11 +1,5 @@
 <template>
-  <section class="screen-wrapper">
-    <div class="screen-header">
-      <a href="javascript:void(0);" onclick="history.back()"
-        ><img :src="ArrowLeftIcon" alt="Arrow Left"
-      /></a>
-      <h3>Settings</h3>
-    </div>
+  <main-layout :title="'Settings'" back-btn>
     <section class="settings-wrapper">
       <div class="settings-header">
         <div class="setting-header-left">
@@ -13,13 +7,13 @@
             <img :src="AvatarIcon" alt="Avatar" />
           </div>
           <div>
-            <h5>Ivan Ivanov</h5>
+            <h5>Etech</h5>
             <p>Connected Via Google</p>
           </div>
         </div>
-        <a href="javascript:void(0)"
-          ><img :src="ArrowRightIcon" alt="Arrow Right"
-        /></a>
+        <a href="javascript:void(0)">
+          <img :src="ArrowRightIcon" alt="Arrow Right"/>
+        </a>
       </div>
       <div class="setting-items">
         <a href="javascript:void(0);" class="setting-item">
@@ -44,17 +38,10 @@
         </a>
       </div>
     </section>
-    <div class="app-screen-footer">
-      <div>
-        <a href="#"><img :src="HomeIcon" alt="Home" /></a>
-        <a href="#"><img :src="SettingsIcon" alt="Settings" /></a>
-      </div>
-    </div>
-  </section>
+  </main-layout>
 </template>
 
 <script setup lang="ts">
-import ArrowLeftIcon from 'components/icons/arrow-left.svg';
 import AvatarIcon from 'components/icons/avatar.svg';
 import ArrowRightIcon from 'components/icons/arrow-right.svg';
 import SubscriptionIcon from 'components/icons/subscription.svg';
@@ -62,8 +49,11 @@ import InstagramIcon from 'components/icons/instagram.svg';
 import SupportIcon from 'components/icons/support.svg';
 import TeamIcon from 'components/icons/team.svg';
 import ShareIcon from 'components/icons/share.svg';
-import HomeIcon from 'components/icons/home.svg';
-import SettingsIcon from 'components/icons/settings.svg';
+
+
+import MainLayout from 'src/layouts/MainLayout.vue';
+
+
 
 defineOptions({
   name: 'SettingPage',
